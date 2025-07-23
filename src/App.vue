e3f45fa3b53ba4db82d3d480095ddd782af2607f
bakedpotatolord <jeffy9745@gmail.com>
2025-07-22 20:51:31 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+import { onMounted } from 'vue'
+
+const canvas = null
+
+onMounted(() => {
+  if(canvas) {
+    
+  }
+})
+
+</script>
+
+<template>
+  <div>
+    
+    <canvas rel="calvas"></canvas>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
